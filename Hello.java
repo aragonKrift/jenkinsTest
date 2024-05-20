f49b18ac9ba79589fe0603ff01946194e27cca5b
Nir <nir.tsameret@gmail.com>
2024-05-20 09:40:17 +0300

    Update again Hello

--- Hello.java
+++ Hello.java
@@ -1,11 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 10; i++) {
-<<<<<<< HEAD
 			System.out.println("Hello World number" + i);
-=======
-			System.out.println("Hello World number " + i);
->>>>>>> 26db07f0d6885bf23141513726a9673e75fd1cf5
 		}
 	}
 }
